f542d4f6b55e9236e6545b135333f08fa62a112d
Enzo Benoit-Jeannin <enzo.benoit-jeannin@mail.mcgill.ca>
2024-04-14 18:21:25 -0400

    A2C (baseline and selfplay)
    Clean up some DDQN runs as well

--- Models/A2C/A2C_Agent.py
+++ Models/A2C/A2C_Agent.py
@@ -1,159 +1,244 @@
-import torch
-import torch.nn.functional as F
-import torch.nn as nn
-from torch.optim import Adam
-from torch.distributions import Categorical
+"""
+Inspired from: https://github.com/ericyangyu/PPO-for-Beginners
+Adapted for to perform self-play (From scratch) on a discrete action-space environment (Originally continuous)
+"""
+
 import numpy as np
-from Models.A2C.MLP import MLP
+import torch
+import torch.nn as nn
+from torch.optim import RMSprop
+from torch.distributions import Categorical
+from Models.PPO.MLP import MLP
 from utils import convert_to_vector
- 
-
-
+    
 class A2C_Agent:
-    def __init__(self, obs_dim, act_dim, DEVICE, lr, eps, gamma, lam, ent_coef, max_grad_norm, mlp_layers, render, timesteps_per_actor=5, n_actors=16, timesteps_per_batch=4096):
+    """
+        This is the A2C class we will use as our model in main.py
+    """
+    def __init__(self, obs_dim, act_dim, DEVICE, \
+                 timesteps_per_batch=5, lr=0.005, eps=1e-5, gamma=0.95, \
+                      ent_coef=0, max_grad_norm=0.5, \
+                        mlp_layers=[64, 64], render=False):
+        """
+            Initializes the A2C model, including hyperparameters.
+        """		
+
+        
+        # Store the hyperparameters
         self.obs_dim = obs_dim
         self.act_dim = act_dim
         self.DEVICE = DEVICE
+        self.timesteps_per_batch = timesteps_per_batch # Number of timesteps to run per batch
         self.lr = lr
-        self.eps = eps
         self.gamma = gamma
-        self.lam = lam
+        self.lam = 1 # GAE lambda is fixed to 1 for A2C
         self.ent_coef = ent_coef
         self.max_grad_norm = max_grad_norm
-        self.mlp_layers = mlp_layers
         self.render = render
-        self.timesteps_per_actor = timesteps_per_actor
-        self.n_actors = n_actors
-        self.timesteps_per_batch = timesteps_per_batch
 
+        # Initialize actor and critic networks
         self.actor = MLP(self.obs_dim, self.act_dim, is_actor=True, DEVICE=DEVICE, fc1_dims=mlp_layers[0], fc2_dims=mlp_layers[1])
         self.critic = MLP(self.obs_dim, 1, is_actor=False, DEVICE=DEVICE, fc1_dims=mlp_layers[0], fc2_dims=mlp_layers[1])
 
-        
-        self.actor_optim = Adam(self.actor.parameters(), lr=self.lr, eps=eps)
-        self.critic_optim = Adam(self.critic.parameters(), lr=self.lr, eps=eps)
-    
- 
-    def save_models(self, folder_path, agent_number, n):
-        self.actor.save_checkpoint(f"{folder_path}/step-{n}-agent-{agent_number}-actor.pt")
-        self.critic.save_checkpoint(f"{folder_path}/step-{n}-agent-{agent_number}-critic.pt") 
-    
-    def load_models(self, folder_path, agent_number, n):
+        # Initialize optimizers for actor and critic, A2C uses RMSprop for both unlike PPO that uses Adam
+        self.actor_optim = RMSprop(self.actor.parameters(), lr=self.lr, eps=eps)
+        self.critic_optim = RMSprop(self.critic.parameters(), lr=self.lr, eps=eps)
 
+    def save_models(self, folder_path, agent_number, n):
+        """
+        Save the models
+        """
+        self.actor.save_checkpoint(f"{folder_path}/step-{n}-agent-{agent_number}-actor.pt")
+        self.critic.save_checkpoint(f"{folder_path}/step-{n}-agent-{agent_number}-critic.pt")  
+        torch.save(self.actor_optim.state_dict(), f"{folder_path}/step-{n}-agent-{agent_number}-actor_optim.pt")
+        torch.save(self.critic_optim.state_dict(), f"{folder_path}/step-{n}-agent-{agent_number}-critic_optim.pt")
+
+    def load_models(self, folder_path, agent_number, n):
+        """
+        Load the models
+        """
         self.actor.load_checkpoint(f"{folder_path}/step-{n}-agent-{agent_number}-actor.pt")
         self.critic.load_checkpoint(f"{folder_path}/step-{n}-agent-{agent_number}-critic.pt")
-
+        self.actor_optim.load_state_dict(torch.load(f"{folder_path}/step-{n}-agent-{agent_number}-actor_optim.pt"))
+        self.critic_optim.load_state_dict(torch.load(f"{folder_path}/step-{n}-agent-{agent_number}-critic_optim.pt"))
+        
     def copy_models(self, agent):
+        """
+        Copy the models
+        """
         self.actor.load_state_dict(agent.actor.state_dict())
         self.critic.load_state_dict(agent.critic.state_dict())
-    
+
     def evaluation_mode(self):
+        """
+        Set the actor to evaluation mode
+        """
         self.actor.eval()
-    
+
     def training_mode(self):
+        """
+        Set the actor to training mode
+        """
         self.actor.train()
-    
 
     def disable_gradients(self):
+        """
+        Disable the gradients
+        """
         for param in self.actor.parameters():
             param.requires_grad = False
         for param in self.critic.parameters():
             param.requires_grad = False
 
-    def learn(self, batch_obs, batch_acts, batch_log_probs, batch_rews, batch_vals, batch_dones, writer, n_steps_so_far):
-        A_k = self.calculate_gae(batch_rews, batch_vals, batch_dones)
+    def learn(self, batch_obs, batch_acts, batch_log_probs, batch_rews,\
+               batch_vals, batch_dones, n_steps_so_far, total_n_steps, writer):
+        """
+            Train the actor and critic networks for 1 iteration given a batch of data.
+        """   
+
+        # Calculate advantage using GAE
+        # Note A2C does not normalize advantages like PPO does
+        A_k = self.calculate_gae(batch_rews, batch_vals, batch_dones) 
 
         V = self.critic(batch_obs).squeeze()
-        batch_rtgs = A_k + V.detach()
+        batch_rtgs = A_k + V.detach()   
 
-    
-        A_k = (A_k - A_k.mean()) / (A_k.std() + 1e-10)
-        
-   
+        # Unlike PPO where we have a fixed number of epochs, in A2C we iterate over the data directly (we don't have a minibatch either)
+        loss = []
+
+        # Learning Rate Annealing
+        frac = (n_steps_so_far - 1.0) / total_n_steps
+        new_lr = self.lr * (1.0 - frac)
+        # Make sure learning rate doesn't go below 0
+        new_lr = max(new_lr, 0.0)
+        self.actor_optim.param_groups[0]["lr"] = new_lr
+        self.critic_optim.param_groups[0]["lr"] = new_lr
+
+        # Log the learning rate as a function of the number of time steps
+        writer.add_scalar('Learning rate value - Training step', new_lr, n_steps_so_far)
+
+        # Calculate V_phi and pi_theta(a_t | s_t) and entropy
         V, curr_log_probs, entropy = self.evaluate(batch_obs, batch_acts)
 
-     
-        logratios = curr_log_probs - batch_log_probs
-        actor_loss = -(logratios * A_k.detach()).mean()
+        # Calculate the loss for the actor and critic networks
+        actor_loss = -(A_k * curr_log_probs).mean()
+        critic_loss = nn.MSELoss()(V, batch_rtgs)
 
-   
-        actor_loss -= self.ent_coef * entropy.mean()
+        # Calulate the entropy loss
+        entropy_loss = entropy.mean()
 
-    
-        critic_loss = F.mse_loss(V, batch_rtgs.detach())
+        # Discount entropy loss by given coefficient
+        actor_loss = actor_loss - self.ent_coef * entropy_loss
 
-      
+        # Calculate gradients and perform backward propagation for actor network
         self.actor_optim.zero_grad()
-        actor_loss.backward()
+        actor_loss.backward(retain_graph=True)
+        # Gradient Clipping with given threshold
         nn.utils.clip_grad_norm_(self.actor.parameters(), self.max_grad_norm)
         self.actor_optim.step()
 
-   
+        # Calculate gradients and perform backward propagation for critic network
         self.critic_optim.zero_grad()
         critic_loss.backward()
         nn.utils.clip_grad_norm_(self.critic.parameters(), self.max_grad_norm)
         self.critic_optim.step()
 
-        writer.add_scalar('Actor loss - Training step', actor_loss.item(), n_steps_so_far)
-        
+        loss.append(actor_loss.detach())
 
-        current_lr = self.actor_optim.param_groups[0]['lr']
-        writer.add_scalar('Learning rate value - Training step', current_lr, n_steps_so_far)
-
+        # Log actor loss
+        avg_loss = sum(loss) / len(loss)
+        writer.add_scalar('Actor loss - Training step', avg_loss.item(), n_steps_so_far)
 
     def calculate_gae(self, rewards, values, dones):
-        batch_advantages = []  
-        #print(rewards.shape, "is the shape of rewards")
-        
+        batch_advantages = []  # List to store computed advantages for each timestep
+
+        # Iterate over each episode's rewards, values, and done flags
         for ep_rews, ep_vals, ep_dones in zip(rewards, values, dones):
-            advantages = []  
-            last_advantage = 0  
-            #print(ep_rews.shape, "is the shape of ep_rews")
-            
-            for t in reversed(range(len(ep_rews))): 
+            advantages = []  # List to store advantages for the current episode
+            last_advantage = 0  # Initialize the last computed advantage
+
+            # Calculate episode advantage in reverse order (from last timestep to first)
+            for t in reversed(range(len(ep_rews))):
                 if t + 1 < len(ep_rews):
-                    
+                    # Calculate the temporal difference (TD) error for the current timestep
                     delta = ep_rews[t] + self.gamma * ep_vals[t+1] * (1 - ep_dones[t+1]) - ep_vals[t]
                 else:
+                    # Special case at the boundary (last timestep)
                     delta = ep_rews[t] - ep_vals[t]
 
-                
+                # Calculate Generalized Advantage Estimation (GAE) for the current timestep
                 advantage = delta + self.gamma * self.lam * (1 - ep_dones[t]) * last_advantage
-                last_advantage = advantage  
-                advantages.insert(0, advantage)  
+                last_advantage = advantage  # Update the last advantage for the next timestep
+                advantages.insert(0, advantage)  # Insert advantage at the beginning of the list
 
+            # Extend the batch_advantages list with advantages computed for the current episode
             batch_advantages.extend(advantages)
 
-    
+        # Convert the batch_advantages list to a PyTorch tensor of type float
         return torch.tensor(batch_advantages, dtype=torch.float).to(self.DEVICE)
 
     def select_action(self, obs, greedy=False):
+        """
+            Queries an action from the actor network, should be called from rollout.
 
+            Parameters:
+                obs - the observation at the current timestep
+
+            Return:
+                action - the action to take, as a numpy array
+                log_prob - the log probability of the selected action in the distribution
+        """
+        # Query the actor network for a mean action
         obs = torch.tensor(obs,dtype=torch.float).to(self.DEVICE)
         probs = self.actor(obs.to(self.DEVICE))
         
-
+        # Create a categorical distribution over the list of probabilities of actions
         dist = Categorical(probs)
 
+        # Sample an action from the distribution
         action = dist.sample()
 
+        # Calculate the log probability for that action
         log_prob = dist.log_prob(action)
 
+        # If we're testing, just return the deterministic action. Sampling should only be for training
+        # as our "exploration" factor.
         if greedy:
             return torch.argmax(probs).item(), 1
 
+        # Return the sampled action and the log probability of that action in our distribution
         return action.detach().item(), log_prob.detach()
 
     def evaluate(self, batch_obs, batch_acts):
+        """
+            Estimate the values of each observation, and the log probs of
+            each action in the most recent batch with the most recent
+            iteration of the actor network. Should be called from learn.
 
+            Parameters:
+                batch_obs - the observations from the most recently collected batch as a tensor.
+                            Shape: (number of timesteps in batch, dimension of observation)
+                batch_acts - the actions from the most recently collected batch as a tensor.
+                            Shape: (number of timesteps in batch, dimension of action)
+                batch_rtgs - the rewards-to-go calculated in the most recently collected
+                                batch as a tensor. Shape: (number of timesteps in batch)
+        """
+        # Query critic network for a value V for each batch_obs. Shape of V should be same as batch_rtgs
+        # if batch_obs.size(0) == 1:
+        #     V = self.critic(batch_obs)
+        # else:
         V = self.critic(batch_obs).squeeze()
 
+        # Calculate the log probabilities of batch actions using most recent actor network.
+        # This segment of code is similar to that in select_action()
         probs = self.actor(batch_obs)
         dist = Categorical(probs)
         log_probs = dist.log_prob(batch_acts)
 
+        # Return the value vector V of each observation in the batch
+        # and log probabilities log_probs of each action in the batch
         return V, log_probs, dist.entropy()
-    
+
     def gather_data(self, env, otherAgent):
        
         batch_obs = []
@@ -230,6 +315,6 @@ class A2C_Agent:
         batch_obs = torch.tensor(batch_obs, dtype=torch.float).to(self.DEVICE)
         batch_acts = torch.tensor(batch_acts, dtype=torch.float).to(self.DEVICE)
         batch_log_probs = torch.tensor(batch_log_probs, dtype=torch.float).flatten().to(self.DEVICE)
-        #print(type(batch_obs), type(batch_acts), type(batch_log_probs), type(batch_rews), type(batch_lens), type(batch_vals), type(batch_dones))
+
         # Here, we return the batch_rews instead of batch_rtgs for later calculation of GAE
         return batch_obs, batch_acts, batch_log_probs, batch_rews, batch_lens, batch_vals, batch_dones
